94d5927a2673fb1f6e6be7fed7f453f8da32ea77
rodgaray <rodgaray@gmail.com>
2015-10-13 18:10:56 -0300

    arreglado

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,24 +4,23 @@ myApp.controller('AppCtrl', ['$scope','$http',function($scope, $http){
 	console.log("Hola desde el controller");
 
 	person1 = {
-		name: 'tim',
-		email: 'tim@email.com'
+		name: 'Tim',
+		email: 'tim@email.com',
 		number: '555555555'
 	};
 
 	person2 = {
-		name: 'emili',
-		email: 'emi@pami.org'
+		name: 'Emily',
+		email: 'emi@pami.org',
 		number: '123123123'
 	};
 
 	person3 = {
-		name: 'rod',
-		email: 'rod@mail.com'
+		name: 'Rod',
+		email: 'rod@mail.com',
 		number: '333333333'
 	};
 
 	var contactlist = [person1,person2,person3]
 	$scope.contactlist = contactlist;
-
 }]);
